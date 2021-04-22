859e3f4a6db7e13d4eac3d4af18f764efd2fd2ef
booshja <jacob.andes@gmail.com>
2021-04-21 22:23:28 -0700

    finished deck-of-cards exercise

---
+++ Node/31-2-JS-Promises/Deck-of-Cards/deck_of_cards.js
@@ -0,0 +1,42 @@
+const url = "https://deckofcardsapi.com/api/deck";
+const gimmeBtn = document.querySelector("#gimme");
+const cardDisplay = document.querySelector("#card-container");
+let deckIdUrl = null;
+let deckID = null;
+
+gimmeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!deckID) {
+        axios.get(url + "/new/draw").then((res) => {
+            deckID = res.data.deck_id;
+            deckIdUrl = url + "/" + deckID;
+            cardDiv = createCard(res.data.cards[0]);
+            cardDiv.style.zIndex = "0";
+            cardDisplay.appendChild(cardDiv);
+        });
+    } else {
+        axios.get(deckIdUrl + "/draw/?count=1").then((res) => {
+            cardDiv = createCard(res.data.cards[0]);
+            zIndex = (51 - res.data.remaining).toString();
+            cardDiv.style.zIndex = zIndex;
+            cardDisplay.appendChild(cardDiv);
+            if (res.data.remaining == 0) {
+                gimmeBtn.style.display = "none";
+            }
+        });
+    }
+});
+
+function degrees() {
+    return Math.floor(Math.random() * 90 + -45);
+}
+
+function createCard(cardObj) {
+    cardImage = cardObj.image;
+    transform = degrees();
+    cardDiv = document.createElement("span");
+    cardDiv.style.backgroundImage = `url("${cardImage}")`;
+    cardDiv.style.transform = `rotate(${transform}deg)`;
+    cardDiv.className = "card-cont__card";
+    return cardDiv;
+}
