b3559e8cb687c13acef33943c6a8e06a6ab00ebe
booshja <jacob.andes@gmail.com>
2021-05-20 15:48:39 -0700

    added documentation to sql.js, updated readme changelog

--- Node/37-Jobly/helpers/sql.js
+++ Node/37-Jobly/helpers/sql.js
@@ -1,20 +1,21 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+    // Create an array from the dataToUpdate object's keys
+    const keys = Object.keys(dataToUpdate);
+    // Throw an error if there is nothing sent to update
+    if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+    const cols = keys.map(
+        (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
+    );
 
-  return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
-  };
+    // Return an object with the columns and values to update in the database, ready to plug in to a SQL query
+    return {
+        setCols: cols.join(", "),
+        values: Object.values(dataToUpdate),
+    };
 }
 
 module.exports = { sqlForPartialUpdate };
--- Node/37-Jobly/readme.md
+++ Node/37-Jobly/readme.md
@@ -5,7 +5,11 @@ This is the Express backend for Jobly, version 2.
 To run this:
 
     node server.js
-    
+
 To run the tests:
 
     jest -i
+
+## Change Log
+
+-   5-20-2021: Added documentation to sql.js
