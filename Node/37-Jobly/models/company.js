24abae2f454363529807f2c2ae4040ff9cfd21a4
booshja <jacob.andes@gmail.com>
2021-05-20 20:37:35 -0700

    finished step 2, filtered search with tests

--- Node/37-Jobly/models/company.js
+++ Node/37-Jobly/models/company.js
@@ -7,140 +7,186 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 /** Related functions for companies. */
 
 class Company {
-  /** Create a company (from data), update db, return new company data.
-   *
-   * data should be { handle, name, description, numEmployees, logoUrl }
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl }
-   *
-   * Throws BadRequestError if company already in database.
-   * */
+    /** Create a company (from data), update db, return new company data.
+     *
+     * data should be { handle, name, description, numEmployees, logoUrl }
+     *
+     * Returns { handle, name, description, numEmployees, logoUrl }
+     *
+     * Throws BadRequestError if company already in database.
+     * */
 
-  static async create({ handle, name, description, numEmployees, logoUrl }) {
-    const duplicateCheck = await db.query(
-          `SELECT handle
-           FROM companies
-           WHERE handle = $1`,
-        [handle]);
+    static async create({ handle, name, description, numEmployees, logoUrl }) {
+        const duplicateCheck = await db.query(
+            `SELECT handle
+            FROM companies
+            WHERE handle = $1`,
+            [handle]
+        );
 
-    if (duplicateCheck.rows[0])
-      throw new BadRequestError(`Duplicate company: ${handle}`);
+        if (duplicateCheck.rows[0])
+            throw new BadRequestError(`Duplicate company: ${handle}`);
 
-    const result = await db.query(
-          `INSERT INTO companies
-           (handle, name, description, num_employees, logo_url)
-           VALUES ($1, $2, $3, $4, $5)
-           RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
-    );
-    const company = result.rows[0];
+        const result = await db.query(
+            `INSERT INTO companies
+            (handle, name, description, num_employees, logo_url)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
+            [handle, name, description, numEmployees, logoUrl]
+        );
+        const company = result.rows[0];
 
-    return company;
-  }
+        return company;
+    }
 
-  /** Find all companies.
-   *
-   * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
-   * */
+    /** Find all companies.
+     *
+     * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
+     * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           ORDER BY name`);
-    return companiesRes.rows;
-  }
+    static async findAll() {
+        const companiesRes = await db.query(
+            `SELECT handle,
+                    name,
+                    description,
+                    num_employees AS "numEmployees",
+                    logo_url AS "logoUrl"
+            FROM companies
+            ORDER BY name`
+        );
+        return companiesRes.rows;
+    }
 
-  /** Given a company handle, return data about company.
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl, jobs }
-   *   where jobs is [{ id, title, salary, equity, companyHandle }, ...]
-   *
-   * Throws NotFoundError if not found.
-   **/
+    /** Find companies with query string filters (name, minEmployees, maxEmployees)
+     *
+     * Returns [{handle, name, description, numEmployees, logoUrl }, ...]
+     */
 
-  static async get(handle) {
-    const companyRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           WHERE handle = $1`,
-        [handle]);
+    static async filterFind(params) {
+        let wheres = [];
+        let values = [];
+        let idx = 1;
 
-    const company = companyRes.rows[0];
+        // Add name to wheres and values if in params
+        if (params.name) {
+            wheres.push(`name ILIKE $${idx}`);
+            values.push("%" + params.name + "%");
+            idx += 1;
+        }
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+        // Add parts for where and values arrays
+        if (params.minEmployees && params.maxEmployees) {
+            if (+params.minEmployees > +params.maxEmployees)
+                throw new BadRequestError(
+                    "minEmployees cannot be greater than maxEmployees."
+                );
 
-    return company;
-  }
+            wheres.push(`num_employees BETWEEN $${idx} AND $${idx + 1}`);
+            values.push(+params.minEmployees, +params.maxEmployees);
+        } else if (params.minEmployees && !params.maxEmployees) {
+            wheres.push(`num_employees >= $${idx}`);
+            values.push(+params.minEmployees);
+        } else if (params.maxEmployees && !params.minEmployees) {
+            wheres.push(`num_employees <= $${idx}`);
+            values.push(+params.maxEmployees);
+        }
 
-  /** Update company data with `data`.
-   *
-   * This is a "partial update" --- it's fine if data doesn't contain all the
-   * fields; this only changes provided ones.
-   *
-   * Data can include: {name, description, numEmployees, logoUrl}
-   *
-   * Returns {handle, name, description, numEmployees, logoUrl}
-   *
-   * Throws NotFoundError if not found.
-   */
+        // Create string from wheres array
+        const sqlWheres = wheres.join(" AND ");
 
-  static async update(handle, data) {
-    const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
+        // Database request
+        const companiesRes = await db.query(
+            `SELECT handle,
+                    name,
+                    description,
+                    num_employees AS "numEmployees",
+                    logo_url AS "logoUrl"
+            FROM companies
+            WHERE ${sqlWheres}`,
+            values
+        );
+        return companiesRes.rows;
+    }
+
+    /** Given a company handle, return data about company.
+     *
+     * Returns { handle, name, description, numEmployees, logoUrl, jobs }
+     *   where jobs is [{ id, title, salary, equity, companyHandle }, ...]
+     *
+     * Throws NotFoundError if not found.
+     **/
+
+    static async get(handle) {
+        const companyRes = await db.query(
+            `SELECT handle,
+                    name,
+                    description,
+                    num_employees AS "numEmployees",
+                    logo_url AS "logoUrl"
+            FROM companies
+            WHERE handle = $1`,
+            [handle]
+        );
+
+        const company = companyRes.rows[0];
+
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
+
+        return company;
+    }
+
+    /** Update company data with `data`.
+     *
+     * This is a "partial update" --- it's fine if data doesn't contain all the
+     * fields; this only changes provided ones.
+     *
+     * Data can include: {name, description, numEmployees, logoUrl}
+     *
+     * Returns {handle, name, description, numEmployees, logoUrl}
+     *
+     * Throws NotFoundError if not found.
+     */
+
+    static async update(handle, data) {
+        const { setCols, values } = sqlForPartialUpdate(data, {
+            numEmployees: "num_employees",
+            logoUrl: "logo_url",
         });
-    const handleVarIdx = "$" + (values.length + 1);
+        const handleVarIdx = "$" + (values.length + 1);
 
-    const querySql = `UPDATE companies 
-                      SET ${setCols} 
-                      WHERE handle = ${handleVarIdx} 
-                      RETURNING handle, 
-                                name, 
-                                description, 
-                                num_employees AS "numEmployees", 
+        const querySql = `UPDATE companies
+                        SET ${setCols}
+                        WHERE handle = ${handleVarIdx}
+                        RETURNING handle,
+                                name,
+                                description,
+                                num_employees AS "numEmployees",
                                 logo_url AS "logoUrl"`;
-    const result = await db.query(querySql, [...values, handle]);
-    const company = result.rows[0];
+        const result = await db.query(querySql, [...values, handle]);
+        const company = result.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
 
-    return company;
-  }
+        return company;
+    }
 
-  /** Delete given company from database; returns undefined.
-   *
-   * Throws NotFoundError if company not found.
-   **/
+    /** Delete given company from database; returns undefined.
+     *
+     * Throws NotFoundError if company not found.
+     **/
 
-  static async remove(handle) {
-    const result = await db.query(
-          `DELETE
-           FROM companies
-           WHERE handle = $1
-           RETURNING handle`,
-        [handle]);
-    const company = result.rows[0];
+    static async remove(handle) {
+        const result = await db.query(
+            `DELETE
+            FROM companies
+            WHERE handle = $1
+            RETURNING handle`,
+            [handle]
+        );
+        const company = result.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
-  }
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
+    }
 }
-
 
 module.exports = Company;
