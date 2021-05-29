06cb064a789d86adfb20aaa5a549d33690ad5ad3
booshja <jacob.andes@gmail.com>
2021-05-28 18:03:10 -0700

    finished part 4 of exercise

--- React/39-5-react-testing/react-state-carousel-coins/carousel/src/Carousel.test.js
+++ React/39-5-react-testing/react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -11,6 +11,46 @@ it("should match snapshot", () => {
     expect(asFragment()).toMatchSnapshot();
 });
 
+it("does not display the left arrow on the first image", () => {
+    const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+    // expect the first image to show, but not the second
+    expect(
+        queryByAltText("Photo by Richard Pasquarella on Unsplash")
+    ).toBeInTheDocument();
+    expect(
+        queryByAltText("Photo by Pratik Patel on Unsplash")
+    ).not.toBeInTheDocument();
+
+    // expect the left arrow not to show on first image, but expect right arrow to show
+    expect(queryByTestId("left-arrow")).not.toBeInTheDocument();
+    expect(queryByTestId("right-arrow")).toBeInTheDocument();
+});
+
+it("does not display the right arrow on the last image", () => {
+    const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+    // move forward to last image of carousel (default is 3 cards)
+    const rightArrow = queryByTestId("right-arrow");
+    fireEvent.click(rightArrow);
+    fireEvent.click(rightArrow);
+
+    // expect the third image to show, not the first two
+    expect(
+        queryByAltText("Photo by Josh Post on Unsplash")
+    ).toBeInTheDocument();
+    expect(
+        queryByAltText("Photo by Richard Pasquarella on Unsplash")
+    ).not.toBeInTheDocument();
+    expect(
+        queryByAltText("Photo by Pratik Patel on Unsplash")
+    ).not.toBeInTheDocument();
+
+    // expect the right arrow to not show, but expect left to show
+    expect(queryByTestId("right-arrow")).not.toBeInTheDocument();
+    expect(queryByTestId("left-arrow")).toBeInTheDocument();
+});
+
 it("works when you click on the right arrow", function () {
     const { queryByTestId, queryByAltText } = render(<Carousel />);
 
