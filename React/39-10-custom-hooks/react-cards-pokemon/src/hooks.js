3cf57813c3f41cc6a387338cf5157fe5a5405c0b
booshja <jacob.andes@gmail.com>
2021-06-02 23:54:09 -0700

    finished part 3 of exercise

--- React/39-10-custom-hooks/react-cards-pokemon/src/hooks.js
+++ React/39-10-custom-hooks/react-cards-pokemon/src/hooks.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import axios from "axios";
+import uuid from "uuid";
 
 const useFlip = () => {
     const [isFacingUp, setIsFacingUp] = useState(true);
@@ -9,4 +11,13 @@ const useFlip = () => {
     return [isFacingUp, toggleIsFacingUp];
 };
 
-export { useFlip };
+const useAxios = (url) => {
+    const [data, setData] = useState([]);
+    const addData = async () => {
+        const res = await axios.get(url);
+        setData((data) => [...data, { ...res.data, id: uuid() }]);
+    };
+    return [data, addData];
+};
+
+export { useFlip, useAxios };
