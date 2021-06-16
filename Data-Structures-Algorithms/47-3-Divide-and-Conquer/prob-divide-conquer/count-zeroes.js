4b773be3079b08adebfd6c5bd53bcff21c1c441b
booshja <jacob.andes@gmail.com>
2021-06-16 16:52:21 -0700

    finished count-zeroes

--- Data-Structures-Algorithms/47-3-Divide-and-Conquer/prob-divide-conquer/count-zeroes.js
+++ Data-Structures-Algorithms/47-3-Divide-and-Conquer/prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,18 @@
-function countZeroes() {
-  
+function countZeroes(input) {
+    let leftIdx = 0;
+    let rightIdx = input.length - 1;
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = input[middleIdx];
+        if (middleVal === 1) {
+            leftIdx = middleIdx + 1;
+        } else if (middleVal === 0 && input[middleIdx - 1] === 0) {
+            rightIdx = middleIdx - 1;
+        } else {
+            return input.length - middleIdx;
+        }
+    }
+    return 0;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
