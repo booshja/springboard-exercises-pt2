0f84e02b4c4c5e3f8300e43b3dcfb43df6230e43
booshja <jacob.andes@gmail.com>
2021-06-16 18:45:22 -0700

    finished sorted-frequency

--- Data-Structures-Algorithms/47-3-Divide-and-Conquer/prob-divide-conquer/sorted-frequency.js
+++ Data-Structures-Algorithms/47-3-Divide-and-Conquer/prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,50 @@
-function sortedFrequency() {
+function sortedFrequency(array, val) {
+    let leftIdx = 0;
+    let leftFoundIdx = null;
+    let rightIdx = array.length - 1;
 
+    while (leftIdx <= rightIdx) {
+        // Find first occurance of val
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = array[middleIdx];
+
+        if (middleVal < val) {
+            leftIdx = middleIdx + 1;
+        } else if (middleVal > val) {
+            rightIdx = middleIdx - 1;
+        } else if (
+            middleVal === val &&
+            (array[middleIdx - 1] === val || undefined)
+        ) {
+            rightIdx = middleIdx - 1;
+        } else if (middleVal === val && array[middleIdx - 1] !== val) {
+            leftFoundIdx = middleIdx;
+            leftIdx = rightIdx + 1;
+        }
+    }
+
+    // value not found, return -1
+    if (leftFoundIdx === null) return -1;
+
+    leftIdx = leftFoundIdx;
+    rightIdx = array.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        // Find last occurance of val
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = array[middleIdx];
+
+        if (middleVal > val) {
+            rightIdx = middleIdx - 1;
+        } else if (
+            middleVal === val &&
+            (array[middleIdx + 1] === val || undefined)
+        ) {
+            leftIdx = middleIdx + 1;
+        } else {
+            return middleIdx - leftFoundIdx + 1;
+        }
+    }
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
