67a67dd4a7cc8d686726ebde244ef0f1614b6c5e
booshja <jacob.andes@gmail.com>
2021-06-18 14:00:00 -0700

    added stack push method

--- Data-Structures-Algorithms/47-5-Stacks-Queues/stack.js
+++ Data-Structures-Algorithms/47-5-Stacks-Queues/stack.js
@@ -1,46 +1,54 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+    /** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+    push(val) {
+        const newNode = new Node(val);
 
-  }
+        // if stack is empty, set new item to be the only one in it
+        if (this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+            this.size = 1;
+            return undefined;
+        }
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+        // set new item to top of stack, update size, return undefined
+        newNode.next = this.first;
+        this.first = newNode;
+        this.size += 1;
+        return undefined;
+    }
 
-  pop() {
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
 
-  }
+    pop() {}
 
-  /** peek(): return the value of the first node in the stack. */
+    /** peek(): return the value of the first node in the stack. */
 
-  peek() {
+    peek() {}
 
-  }
+    /** isEmpty(): return true if the stack is empty, otherwise false */
 
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+    isEmpty() {}
 }
 
 module.exports = Stack;
