a820560f321b46400829457bcaacd0369a399770
booshja <jacob.andes@gmail.com>
2021-06-25 21:55:05 -0700

    finished merge sort methods

--- Data-Structures-Algorithms/47-11-Sorting/merge.js
+++ Data-Structures-Algorithms/47-11-Sorting/merge.js
@@ -1,5 +1,33 @@
-function merge() {}
+function merge(arr1, arr2) {
+    const results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
 
-function mergeSort() {}
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    const mid = Math.floor(array.length / 2);
+    const left = mergeSort(array.slice(0, mid));
+    const right = mergeSort(array.slice(mid));
+    return merge(left, right);
+}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
