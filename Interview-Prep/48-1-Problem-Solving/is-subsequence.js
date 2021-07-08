0159044305362a7319a6150d4744505502b57ed7
booshja <jacob.andes@gmail.com>
2021-07-07 22:27:15 -0700

    finished is-subsequence

--- Interview-Prep/48-1-Problem-Solving/is-subsequence.js
+++ Interview-Prep/48-1-Problem-Solving/is-subsequence.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+/** Write a function called isSubsequence which takes in two strings
+ * and checks whether the characters in the first string form a subsequence
+ * of the characters in the second string. In other words, the function should
+ * check whether the characters in the first string appear somewhere in the
+ * second string, without their order changing.
+ *
+ * Constraints: Time Complexity - O(N + M)
+ **/
+
+function isSubsequence(str1, str2) {
+    let str1Idx = 0;
+    let str2Idx = 0;
+
+    if (!str1) return true;
+
+    while (str2Idx < str2.length) {
+        if (str2[str2Idx] === str1[str1Idx]) {
+            str1Idx += 1;
+        }
+
+        if (str1Idx === str1.length) return true;
+        str2Idx += 1;
+    }
+
+    return false;
 }
