74914b81a8381c1fe721d4ed9702b129c734291d
booshja <jacob.andes@gmail.com>
2021-07-08 16:48:36 -0700

    finished guessing-game

--- Interview-Prep/48-3-JS-Tricky-Parts/guessing-game.js
+++ Interview-Prep/48-3-JS-Tricky-Parts/guessing-game.js
@@ -1,5 +1,20 @@
 function guessingGame() {
+    let guesses = 0;
+    let hasWon = false;
+    const target = Math.floor(Math.random() * 100);
 
+    return function (guess) {
+        if (hasWon) return "The game is over, you already won!";
+        guesses++;
+        if (guess === target) {
+            hasWon = true;
+            return `You win! You found ${target} in ${guesses} guesses.`;
+        } else if (guess > target) {
+            return `${guess} is too high!`;
+        } else if (guess < target) {
+            return `${guess} is too low!`;
+        }
+    };
 }
 
 module.exports = { guessingGame };
