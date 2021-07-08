7ddb0d207e732ee551362b4ceae1b35a971e85ac
booshja <jacob.andes@gmail.com>
2021-07-07 20:50:02 -0700

    finished average-pair

--- Interview-Prep/48-1-Problem-Solving/average-pair.js
+++ Interview-Prep/48-1-Problem-Solving/average-pair.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+/** Write a function called averagePair.
+ *
+ * Given a sorted array of integers and a target average,
+ * determine if there is a pair of values in the array where
+ * the average of the pair equals the target average.
+ * There may be more than one pair that matches the average target.
+ *
+ * Constraints: Time Complexity: O(N)
+ **/
+
+function averagePair(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const avg = (nums[left] + nums[right]) / 2;
+        if (avg === target) {
+            return true;
+        } else if (avg > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return false;
 }
