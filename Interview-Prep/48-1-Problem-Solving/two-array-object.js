bf79e51ec22aa195e5b97d8018e24904203e5600
booshja <jacob.andes@gmail.com>
2021-07-07 21:05:15 -0700

    finished two-array-object

--- Interview-Prep/48-1-Problem-Solving/two-array-object.js
+++ Interview-Prep/48-1-Problem-Solving/two-array-object.js
@@ -1,3 +1,17 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+/** Write a function called twoArrayObject which accepts
+ * two arrays of varying lengths.The first array consists of keys and
+ * the second one consists of values.
+ * Your function should return an object created from the keys and values.
+ * If there are not enough values, the rest of keys should have a value of null.
+ * If there not enough keys, just ignore the rest of values.
+ * */
+
+function twoArrayObject(keysArr, valsArr) {
+    const zippedObj = {};
+
+    for (let i = 0; i < keysArr.length; i++) {
+        zippedObj[keysArr[i]] = valsArr[i] || null;
+    }
+
+    return zippedObj;
 }
