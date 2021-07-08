583d72fc8a8ccc42ecdc5958d700731ca558c42b
booshja <jacob.andes@gmail.com>
2021-07-07 22:32:15 -0700

    finished exercise

--- Interview-Prep/48-1-Problem-Solving/count-pairs.js
+++ Interview-Prep/48-1-Problem-Solving/count-pairs.js
@@ -1,3 +1,28 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+/** Given an array of integers, and a targetSumber, find the targetSumber of
+ * pairs of integers in the array whose sum is equal to the second
+ * parameter. You can assume that there will be no duplicate values
+ * in the array.
+ *
+ * Constraints: Time Complexity - O(N * log(N)) or O(N)
+ * */
+
+function countPairs(arr, targetSum) {
+    arr.sort((a, b) => a - b);
+    let total = 0;
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start < end) {
+        let sum = arr[start] + arr[end];
+        if (sum === targetSum) {
+            total++;
+            start++;
+            end--;
+        } else if (sum < targetSum) {
+            start++;
+        } else {
+            end--;
+        }
+    }
+    return total;
 }
