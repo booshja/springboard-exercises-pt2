fe9be840fb2ca0614140160cfb7a9193f5398aee
booshja <jacob.andes@gmail.com>
2021-07-08 17:02:54 -0700

    finished create-account

--- Interview-Prep/48-3-JS-Tricky-Parts/create-account.js
+++ Interview-Prep/48-3-JS-Tricky-Parts/create-account.js
@@ -1,5 +1,41 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+    return {
+        checkBalance(enteredPin) {
+            if (enteredPin !== pin) {
+                return "Invalid PIN.";
+            } else {
+                return `$${amount}`;
+            }
+        },
+        deposit(enteredPin, depositAmt) {
+            if (enteredPin !== pin) {
+                return "Invalid PIN.";
+            } else {
+                amount = amount + depositAmt;
+                return `Succesfully deposited $${depositAmt}. Current balance: $${amount}.`;
+            }
+        },
+        withdraw(enteredPin, amt) {
+            if (enteredPin !== pin) {
+                return "Invalid PIN.";
+            } else {
+                if (amt > amount) {
+                    return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+                } else {
+                    amount = amount - amt;
+                    return `Succesfully withdrew $${amt}. Current balance: $${amount}.`;
+                }
+            }
+        },
+        changePin(enteredPin, newPin) {
+            if (enteredPin !== pin) {
+                return "Invalid PIN.";
+            } else {
+                pin = newPin;
+                return "PIN successfully changed!";
+            }
+        },
+    };
 }
 
 module.exports = { createAccount };
