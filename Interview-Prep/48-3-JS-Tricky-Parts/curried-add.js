929154de8be6cee4aec1e1c201be4b21487d08f9
booshja <jacob.andes@gmail.com>
2021-07-08 17:09:20 -0700

    finished exercise

--- Interview-Prep/48-3-JS-Tricky-Parts/curried-add.js
+++ Interview-Prep/48-3-JS-Tricky-Parts/curried-add.js
@@ -1,5 +1,10 @@
 function curriedAdd(total) {
-
+    if (total === undefined) return 0;
+    return function curry(num) {
+        if (num === undefined) return total;
+        total += num;
+        return curry;
+    };
 }
 
 module.exports = { curriedAdd };
